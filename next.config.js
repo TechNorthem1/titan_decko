dfa5ec4ae64773c1765218ea11a04ed4e67caf8a
TechNorthem1 <techtitangroupsas@gmail.com>
2023-12-07 15:49:59 -0500

    update file next.config.js

--- next.config.js
+++ next.config.js
@@ -18,22 +18,6 @@ const nextConfig = {
     // Available on both server and client
     theme: "DEFAULT",
   },
-
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Activar source maps en producción
-    if (!dev) {
-      config.devtool = 'source-map';
-
-      for (const plugin of config.plugins) {
-        if (plugin.constructor.name === 'TerserPlugin') {
-          plugin.options.sourceMap = true;
-          break;
-        }
-      }
-    }
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
